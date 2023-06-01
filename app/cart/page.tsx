6df5b7f0a7a1ed51a56d8b0afa10a99e7bc30f1e
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-06-01 16:14:30 +0200

    add cart

---
+++ app/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className={"cart__container"}>
+      <h2>Cart</h2>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function Navbar() {
   return (
@@ -6,6 +7,15 @@ function Navbar() {
       <a href="/">
         <img src="https://see.fontimg.com/api/renderfont4/OoJA/eyJyIjoiZnMiLCJoIjo1MiwidyI6MjAwMCwiZnMiOjI2LCJmZ2MiOiIjMjkyMzIzIiwiYmdjIjoiIzU4MzYzNiIsInQiOjF9/QmVlcg/806-typography-806-typography.png" />
       </a>
+      <Link href={"/cart"}>
+        <Image
+          src={"cart.svg"}
+          alt="shopping cart"
+          height={40}
+          width={40}
+          className="cart"
+        />
+      </Link>
     </nav>
   );
 }
