b3626dedd6a5c4639064b292b498822ee1de8da1
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-08-02 12:47:28 +0200

    suspense tests

--- app/ale/page.tsx
+++ app/ale/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import CustomImage from "../../src/components/customImg/customImage";
 import { Beer } from "../../types/types";
 import PaginationControls from "@/src/components/PagginationControls/PagginationControls";
+import { Suspense } from "react";
 
 const fetchBeer = async () => {
   const res = await fetch("https://api.sampleapis.com/beers/ale");
@@ -14,17 +15,19 @@ const Stout = async ({
     const beers = await fetchBeer();
   
     const page = searchParams['page'] ?? '1'
-    const per_page = searchParams['per_page'] ??'5'
+    const per_page = searchParams['per_page'] ?? '5'
     
     const start = (Number(page) - 1) * (Number(per_page));
     const end = start + Number(per_page) ;
 
     const enteries = beers.slice(start, end);
+    console.log(enteries)
 
   return (
   <>
+  <Suspense fallback={<p>Loading...</p>}>
     {enteries.map((b) => (
-    <Link className="beers" href={`/ale/${b.id}`} key={b.id}>
+      <Link className="beers" href={`/ale/${b.id}`} key={b.id}>
       <article className="data">
         <h2 className="data__number">{b.id}</h2>
         <p className="data__name">{b.name}</p>
@@ -38,6 +41,7 @@ const Stout = async ({
         unoptimized />
     </Link>
   ))}
+  </Suspense>
   <PaginationControls hasNextPage={end < beers.length} hasPrevPage={start > 0} numberOfItmes={beers.length} beerType={'ale'}/>
   </>
 )};
