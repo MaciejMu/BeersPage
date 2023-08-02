2648501e86c396773ef3d738c324fe8aaf6a5650
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-08-02 12:25:13 +0200

    add paggination

--- app/ale/page.tsx
+++ app/ale/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import CustomImage from "../../src/components/customImg/customImage";
 import { Beer } from "../../types/types";
+import PaginationControls from "@/src/components/PagginationControls/PagginationControls";
 
 const fetchBeer = async () => {
   const res = await fetch("https://api.sampleapis.com/beers/ale");
@@ -8,9 +9,20 @@ const fetchBeer = async () => {
   return beer;
 };
 
-const Stout = async () => {
-  const beer = await fetchBeer();
-  return beer.map((b) => (
+const Stout = async ({
+  searchParams} : { searchParams : {[key:string] : string | string[] | undefined}}) => {
+    const beers = await fetchBeer();
+  
+    const page = searchParams['page'] ?? '1'
+    const per_page = searchParams['per_page'] ??'5'
+    
+    const start = (Number(page) - 1) * (Number(per_page));
+    const end = start + Number(per_page) ;
+
+    const enteries = beers.slice(start, end);
+
+  return enteries.map((b) => (
+    <>
     <Link className="beers" href={`/ale/${b.id}`} key={b.id}>
       <article className="data">
         <h2 className="data__number">{b.id}</h2>
@@ -22,9 +34,10 @@ const Stout = async () => {
         alt="beer image"
         width={150}
         height={150}
-        unoptimized
-      />
+        unoptimized />
     </Link>
+    <PaginationControls hasNextPage={end < beers.length} hasPrevPage={start > 0} numberOfItmes={beers.length} />
+    </>
   ));
 };
 
